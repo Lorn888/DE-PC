5ed15daf1c2a904f0260ead1b431bc32df1089d0
Patryk Okpara <patrykokpara@hotmail.com>
2024-05-09 22:22:16 +0100

    Strings

--- PC-Python-practice/hello.py
+++ PC-Python-practice/hello.py
@@ -1,1 +1,11 @@
 print("helo world!")
+
+position = (12.33, -122.34)  
+colours = ["Cyan", "Magenta", "Yellow", "Black"] 
+numbers = {1, 2, 3, 4, 5, 6}
+
+print(position)
+print(colours)
+print(numbers)
+
+z
---
+++ PC-Python-practice/python-1-exercises.py
@@ -0,0 +1,21 @@
+# Part 1
+
+# Strings
+
+# 1)
+
+name = "Patryk "
+
+print(name)
+
+# 2)
+
+last_name = "Okpara"
+
+full_name = name + last_name
+
+print(full_name)
+
+# 3)
+
+print(f"Hi, my name is {name}{last_name}")
