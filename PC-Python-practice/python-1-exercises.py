d036eb8fc7ecd66b08d25d55d23d5995b03dfd5c
Patryk Okpara <patrykokpara@hotmail.com>
2024-05-09 22:50:18 +0100

    Input and Numbers

--- PC-Python-practice/python-1-exercises.py
+++ PC-Python-practice/python-1-exercises.py
@@ -61,3 +61,32 @@ print(people)
 equal_or_no = people[0] == people[-1]
 
 print(equal_or_no)
+
+# Input
+
+# 1)
+
+# name = input("what is your name ?")
+
+# print(name)
+
+# 2)
+
+# int1 = int(input("first number"))
+# int2 = int(input("second number"))
+
+# product = int1 * int2
+
+# print(product)
+
+# 3)
+
+int_num = int(input("number"))
+
+if int_num % 3 == 0 and int_num % 5 == 0:
+    print ("FizzBuzz")
+elif int_num % 3 == 0:
+    print ("fizz")
+elif int_num % 5 == 0:
+    print ("buzz")
+
